cee1f7d9fc15cef048fca0032c1b465b71e30144
ilia Isaev <ilyadevwk@gmail.com>
2025-02-04 10:49:36 +0400

    feat: styled enter page

--- InteractiveCV/src/app/components/main-page/main-page.component.ts
+++ InteractiveCV/src/app/components/main-page/main-page.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   imports: [],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.less'
+   styleUrl: './main-page.component.less'
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  text: string[] = [
+    "\nWelcome to My Retro Resume!",
+    "Loading profile...",
+    "Name: John Doe",
+    "Skills: JavaScript, Python, HTML, CSS, React",
+    "Experience: 5+ years in web development",
+    "Type 'help' for available commands."
+  ];
+  
+  index = 0;
+  consoleText = '';
 
+  ngOnInit() {
+    this.typeLine();
+  }
+
+  typeLine() {
+    if (this.index < this.text.length) {
+      this.consoleText += this.text[this.index] + "\n";
+      this.index++;
+      setTimeout(() => this.typeLine(), 1000);
+    }
+  }
 }
