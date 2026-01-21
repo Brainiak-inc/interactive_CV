b304833e66f1365f0b1941a3d05738dc8d5ce7f8
Ilia Isaev <i.isaev@mob-edu.ru>
2026-01-21 19:57:16 +0400

    refactored: changing work layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,28 @@ import { Routes } from '@angular/router';
 import { CommandlineComponent } from './components/command-line/commandline.component';
 
 export const routes: Routes = [
-    {path: '', loadComponent: () => 
+    {path: '', loadComponent: () =>
       import('./components/command-line/commandline.component')
         .then(m => m.CommandlineComponent),
       children: [
         {path: 'work',
-          loadComponent: () => 
+          loadComponent: () =>
             import('./components/work-experience-layout/work-experience-layout.component')
               .then(m => m.WorkExperienceLayoutComponent)
         },
         {
           path: 'skills',
-          loadComponent: () => 
-            import('./components/skills-layout/skills-layout/skills-layout.component')
+          loadComponent: () =>
+            import('./components/skills-layout/skills-layout.component')
               .then(m => m.SkillsLayoutComponent)
+        },
+        {
+          path: 'education',
+          loadComponent: () =>
+            import('./components/education-layout/education-layout.component')
+              .then(m => m.EducationLayoutComponent)
         }
       ]
     },
-    
+
 ];
---
+++ src/app/components/education-layout/education-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education-layout',
+  imports: [],
+  templateUrl: './education-layout.component.html',
+  styleUrl: './education-layout.component.less'
+})
+export class EducationLayoutComponent {
+
+}
